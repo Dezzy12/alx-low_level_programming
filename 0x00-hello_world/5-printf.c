969cdf0871ba3affac014376d72fedfa41fd97d8
Dezzy12 <desmondneba134@gmail.com>
2023-09-07 19:26:34 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, 0 is used in almost all cases
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
