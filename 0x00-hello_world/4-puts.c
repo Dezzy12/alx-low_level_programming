f7697cabc2a371abffde78ed8f5c86db7aba1336
Dezzy12 <desmondneba134@gmail.com>
2023-09-07 19:23:08 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main(void){
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, 0 is used in almost all cases
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
 }
