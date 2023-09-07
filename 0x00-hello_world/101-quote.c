557a83443611630b46cf4d754dcf1794e9bb5859
Dezzy12 <desmondneba134@gmail.com>
2023-09-07 20:06:14 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, which is 1
+*/
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59");
+	return (1);
+}
