01724fe9d3e1519c184e6a11f5800b4aa74162da
Dezzy12 <desmondneba134@gmail.com>
2023-09-08 23:10:51 +0100

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, 0 is used in almost all cases
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
