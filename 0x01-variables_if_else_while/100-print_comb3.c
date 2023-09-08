167a5a5c99c31709e88bceb055b0670cba677f35
Dezzy12 <desmondneba134@gmail.com>
2023-09-08 23:21:59 +0100

    advanced##

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, 0 is used in almost all cases
+*/
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
