9e9d3be9c6f1e4dbff1fb0c7558d18a25581b5ce
Dezzy12 <desmondneba134@gmail.com>
2023-09-08 23:03:27 +0100

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Represents the entry point of our C program
+* Return: This is the return value of program, 0 is used in almost all cases
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
