c0cbcbb640462dd848efc14199c7e93d72e8ece0
Dezzy12 <desmondneba134@gmail.com>
2023-09-15 00:32:56 +0100

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,20 +2,20 @@
 
 /**
 * print_square - draws a square
-* @n : number of times the character is to be printed
+* @size : number of times the character is to be printed
 * Return: void
 */
 
-void print_square(int n)
+void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j <= size; j++)
 		{
 			_putchar("#");
 		}
