21fc273874cc7b1c6f8141108c210de4c23f09f7
Dezzy12 <desmondneba134@gmail.com>
2023-09-15 01:09:06 +0100

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,21 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10 != 0)
+		n1 = -n;
+	} else
 	{
-		print_number(n / 10);
+		n1 = n;
 	}
 
-	_putchar('0' + (n % 10));
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar('0' + (n1 % 10));
 }
